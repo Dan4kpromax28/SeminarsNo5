3b5c39dd610ba7854b5ae3aeec4ab9841e0de87d
Dan4kpromax28 <150060065+Dan4kpromax28@users.noreply.github.com>
2024-04-04 11:02:01 +0300

    html add

--- src/main/java/lv/venta/controller/FirstController.java
+++ src/main/java/lv/venta/controller/FirstController.java
@@ -1,6 +1,7 @@
 package lv.venta.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -10,4 +11,12 @@ public class FirstController {
         System.out.println("Hello from string");
         return "hello-page"; //tiek paradita hello-page.html lapa
     }
+
+    @GetMapping("/hello/msg") //localhost:8080/hello/msg
+    public String getHelloMsg(Model model){
+                                        //nosaukums          vērtība
+        model.addAttribute("mymsg","Ziņa no backend!! Hei");
+        return "msg-page";
+    }
+
 }
