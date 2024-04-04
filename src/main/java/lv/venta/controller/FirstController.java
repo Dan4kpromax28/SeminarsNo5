cb2c75aa3c5d1b92e6c96bb1540661b3a15b798b
Dan4kpromax28 <150060065+Dan4kpromax28@users.noreply.github.com>
2024-04-04 10:42:59 +0300

    new commit

---
+++ src/main/java/lv/venta/controller/FirstController.java
@@ -0,0 +1,13 @@
+package lv.venta.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+    @GetMapping("/hello") //
+    public String getHello(){
+        System.out.println("Hello from string");
+        return "hello-page"; //tiek paradita hello-page.html lapa
+    }
+}
