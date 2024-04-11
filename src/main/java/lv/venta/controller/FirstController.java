a2821668f45da5d93886f32aac77ef9afc9c0586
Dan4kpromax28 <150060065+Dan4kpromax28@users.noreply.github.com>
2024-04-11 11:22:50 +0300

    New commit

--- src/main/java/lv/venta/controller/FirstController.java
+++ src/main/java/lv/venta/controller/FirstController.java
@@ -1,6 +1,11 @@
 package lv.venta.controller;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.SneakyThrows;
 import lv.venta.model.Product;
+import lv.venta.service.ICRUDProductSevice;
+import lv.venta.service.IFilterProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,10 +17,12 @@ import java.util.Arrays;
 
 @Controller
 public class FirstController {
-    Product product1 = new Product("Abols", "Labais", 0.87f, 5);
-    Product product2 = new Product("Ediens", "Labais", 0.54f,4);
-    Product product3 = new Product("Ediens", "Labais", 0.54f,4);
-    ArrayList<Product> newList = new ArrayList<Product>(Arrays.asList(product1, product2, product3));
+    @Autowired
+    private ICRUDProductSevice crudService;
+
+    @Autowired
+    private IFilterProductService filterService;
+
     @GetMapping("/hello") //
     public String getHello() {
         System.out.println("Hello from string");
@@ -29,44 +36,56 @@ public class FirstController {
         return "msg-page";
     }
 
+    @SneakyThrows
     @GetMapping("/product/test")
     public String getProductTest(Model model) {
         Product myProduct = new Product("Abols", "Sarkans", 0.99f, 5);
-        model.addAttribute("myobj", myProduct);
-        return "show-product-page";
+        try {
+            model.addAttribute("myobj", crudService.retrieveAll().get(0));
+            return "show-product-page";
+        }
+        catch (Exception e) {
+            model.addAttribute("msg", e.getMessage());
+            return "error-page";
+        }
+
     }
 
     @GetMapping("/product/list") //static
     public String getProductsList(Model model) {
 
 
-        model.addAttribute("myobjs", newList);
-        return "show-product-list";
+        try {
+            model.addAttribute("myobjs", crudService.retrieveAll());
+            return "show-product-list";
+        } catch (Exception e) {
+            model.addAttribute("msg", e.getMessage());
+            return "error-page";
+        }
+
     }
 
     @GetMapping("/productone") //localhost:8080/productone?id=3
     public String getProductone(@RequestParam("id") int id, Model model) {
-        for(Product tempP : newList) {
-            if(tempP.getId() == id) {
-                model.addAttribute("myobj", tempP);
-                return "show-product-page";
-            }
+        try {
+            model.addAttribute("myobj", crudService.retrieveById(id));
+            return "show-product-page";
+        } catch (Exception e) {
+            model.addAttribute("msg", "Wrong id");
+            return "error-page";
         }
-        model.addAttribute("msg", "Wrong id");
-        return "error-page";
+
     }
 
-    @GetMapping("/productone/all/{id}")
+    @GetMapping("/product/all/{id}")
     public String getProductById(@PathVariable("id") int id, Model model) {
-        for(Product tempP : newList) {
-            if(tempP.getId() == id) {
-                model.addAttribute("myobj", tempP);
-                return "show-product-page";
-
-            }
+        try {
+            model.addAttribute("myobj", crudService.retrieveById(id));
+            return "show-product-page";
+        } catch (Exception e) {
+            model.addAttribute("msg", "Wrong id");
+            return "error-page";
         }
-        model.addAttribute("msg", "Wrong id");
-        return "error-page";
     }
 
 }
--- src/main/java/lv/venta/service/impl/ProductServiceImpl.java
+++ src/main/java/lv/venta/service/impl/ProductServiceImpl.java
@@ -1,51 +1,128 @@
 package lv.venta.service.impl;
 
+import jakarta.validation.constraints.NotNull;
 import lv.venta.model.Product;
 import lv.venta.service.ICRUDProductSevice;
 import lv.venta.service.IFilterProductService;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 @Service
 public class ProductServiceImpl implements ICRUDProductSevice, IFilterProductService {
+
+    private ArrayList<Product> allProducts = new ArrayList<>(Arrays.asList(new Product("Daniels", "Balika", 0.99f,1),new Product("Danis", "Bika", 0.99f,6), new Product("iels", "ika", 0.99f,8)));
+
     @Override
+
     public Product crateProduct(Product product) throws Exception {
+        if (product == null) {
+            throw new Exception("Error: product is null");
+        }
+        for (Product tempP : allProducts) {
+            if (tempP.getTitle().equals(product.getTitle()) && tempP.getPrice() == product.getPrice() && tempP.getDescription().equals(product.getDescription())) {
+                tempP.setQuantity(tempP.getQuantity() + product.getQuantity());
+                return tempP;
+
+            }
+        }
         return null;
     }
 
     @Override
     public ArrayList<Product> retrieveAll() throws Exception {
-        return null;
+        if (allProducts.isEmpty()) {
+            throw new Exception("Error: there are no products");
+        }
+        return allProducts;
     }
 
     @Override
     public Product retrieveById(int id) throws Exception {
-        return null;
+        if (allProducts.isEmpty()) {
+            throw new Exception("Error: there are no products");
+        }
+        for (Product tempP : allProducts) {
+            if (tempP.getId() == id) {
+                return tempP;
+            }
+        }
+        throw new Exception("Error: there are no products");
     }
 
     @Override
     public void updateById(int id, Product product) throws Exception {
-
+        if (allProducts.isEmpty()) {
+            throw new Exception("Error: there are no products");
+        }
+        for (Product tempP : allProducts) {
+            if (tempP.getId() == id) {
+                tempP.setTitle(product.getTitle());
+                tempP.setPrice(product.getPrice());
+                tempP.setDescription(product.getDescription());
+                tempP.setQuantity(product.getQuantity());
+                return;
+            }
+        }
+        throw new Exception("Error: there are no products");
     }
 
     @Override
     public Product deleteById(int id) throws Exception {
-        return null;
+        Product product = retrieveById(id);
+        allProducts.remove(product);
+        return product;
     }
 
     @Override
     public ArrayList<Product> filterProductByPriceThreshold(float priceThreshold) throws Exception {
-        return null;
+        if (priceThreshold <= 0 || priceThreshold > 10000) {
+            throw new Exception("PriceThreshold should be between 0 and 10000");
+        }
+        if (allProducts.isEmpty()) {
+            throw new Exception("Error: there are no products");
+        }
+        ArrayList<Product> filteredProducts = new ArrayList<>();
+        for (Product tempP : allProducts) {
+            if (tempP.getPrice() <= priceThreshold) {
+                filteredProducts.add(tempP);
+            }
+        }
+        return filteredProducts;
     }
 
     @Override
     public ArrayList<Product> filterProductByQuantityThreshold(int quantityThreshold) throws Exception {
-        return null;
+        if (quantityThreshold <= 0 || quantityThreshold > 10000) {
+            throw new Exception("QuantityThreshold should be between 0 and 10000");
+        }
+        if (allProducts.isEmpty()) {
+            throw new Exception("Error: there are no products");
+        }
+        ArrayList<Product> filteredProducts = new ArrayList<>();
+        for (Product tempP : allProducts) {
+            if (tempP.getQuantity() <= quantityThreshold) {
+                filteredProducts.add(tempP);
+            }
+        }
+        return filteredProducts;
     }
 
     @Override
     public ArrayList<Product> filterByTitleDescription(String titleDescription) throws Exception {
-        return null;
+        if (titleDescription == null) {
+            throw new Exception("titleDescription is null");
+        }
+        if (allProducts.isEmpty()) {
+            throw new Exception("Error: there are no products");
+        }
+        ArrayList<Product> filteredProducts = new ArrayList<>();
+        for (Product tempP : allProducts) {
+            if (tempP.getTitle().contains(titleDescription) || tempP.getDescription().contains(titleDescription)) {
+                filteredProducts.add(tempP);
+            }
+        }
+        return filteredProducts;
     }
 }
