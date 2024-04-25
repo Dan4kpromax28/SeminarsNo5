7b8e0817228d5d76e907a7bd88bf2135840eef77
Dan4kpromax28 <150060065+Dan4kpromax28@users.noreply.github.com>
2024-04-25 11:00:47 +0300

    r class add

--- src/main/java/lv/venta/controller/ProductFilterAndStatController.java
+++ src/main/java/lv/venta/controller/ProductFilterAndStatController.java
@@ -1,12 +1,60 @@
 package lv.venta.controller;
-
-
+import lv.venta.model.Product;
+import lv.venta.service.IFilterProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping("/product/info")
 public class ProductFilterAndStatController {
+    @Autowired
+    private IFilterProductService filterService;
+
+    @GetMapping("filter/price")
+    public String getProductInfoFilterByPrice(@RequestParam("treshold") float treshold, Model model) {
+        try {
+            ArrayList<Product> result = filterService.filterProductByPriceThreshold(treshold);
+            model.addAttribute("XXX", result);
+            return "XXX";
+
+        } catch (Exception e) {
+            model.addAttribute("msg", e.getMessage());
+            return "error-page";
+        }
+    }
+
+    @GetMapping("filter/quantity")
+    public String getProductInfoFilterByQuantity(@RequestParam("treshold") int treshold, Model model) {
+        try {
+            ArrayList<Product> result = filterService.filterProductByQuantityThreshold(treshold);
+            model.addAttribute("myobjs", result);
+            return "show-product-list";
+
+        } catch (Exception e) {
+            model.addAttribute("msg", e.getMessage());
+            return "error-page";
+        }
+    }
+
+    @GetMapping("filter/text")
+    public String getProductInfoFilterByTitleOrDescription(@RequestParam("keywords") String keywords, Model model) {
+        try {
+            ArrayList<Product> result = filterService.filterByTitleDescription("keywords");
+            model.addAttribute("myobjs", result);
+            return "show-product-list";
+
+        } catch (Exception e) {
+            model.addAttribute("msg", e.getMessage());
+            return "error-page";
+        }
+    }
 
 
 }
