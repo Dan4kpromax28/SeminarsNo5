f0f76a3ea15935a171f9fb811846e7e4a96e0835
Dan4kpromax28 <150060065+Dan4kpromax28@users.noreply.github.com>
2024-05-16 09:32:47 +0300

    New Commit

--- src/main/java/lv/venta/config/WebSecurityConfig.java
+++ src/main/java/lv/venta/config/WebSecurityConfig.java
@@ -2,27 +2,31 @@ package lv.venta.config;
 
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configurable
+@Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfiguration{
+public class WebSecurityConfig { // without extends something and this will all work fine
 
 
     @Bean
     public UserDetailsService testUsers() {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
         UserDetails u1Details =
                 User
                         .builder()
                         .username("admin")
-                        .password("123456")
+                        .password(encoder.encode("123456"))
                         .authorities("ADMIN")
                         .build();
 
@@ -30,14 +34,14 @@ public class WebSecurityConfig extends WebSecurityConfiguration{
                 User
                         .builder()
                         .username("karina")
-                        .password("654321")
+                        .password(encoder.encode("123456"))
                         .authorities("USER")
                         .build();
 
         UserDetails u3Details =
                 User
                         .builder()
-                        .username("janis")
+                        .username(encoder.encode("janis"))
                         .password("098765")
                         .authorities("USER", "ADMIN")
                         .build();
@@ -50,20 +54,21 @@ public class WebSecurityConfig extends WebSecurityConfiguration{
     public SecurityFilterChain configureEndpoints(HttpSecurity http) throws Exception {
 
         http
-                .authorizeHttpRequests()
+                .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/hello").permitAll()
                 .requestMatchers("/hello/msg").permitAll()
                 .requestMatchers("/product/test").hasAuthority("ADMIN")//tikai adminam
-                .requestMatchers("/product/all").permitAll()
-                .requestMatchers("/product/one?id=**").permitAll()
+                .requestMatchers("/product/list").permitAll()
+                .requestMatchers("/product/productone**").permitAll()
                 .requestMatchers("/product/all/**").permitAll()
                 .requestMatchers("/product/insert").hasAuthority("ADMIN")
-                .requestMatchers("/product/update?id=**").hasAuthority("ADMIN")
-                .requestMatchers("/product/delete?id=**").hasAuthority("ADMIN")
+                .requestMatchers("/product/update**").hasAuthority("ADMIN")
+                .requestMatchers("/product/delete**").hasAuthority("ADMIN")
                 .requestMatchers("/product/info/filter/**").hasAuthority("USER")
                 .requestMatchers("/product/info/total").hasAuthority("ADMIN")
-                .and()
-                .formLogin().permitAll();
+                );
+
+        http.formLogin(form -> form.permitAll());
 
 
         return http.build();
